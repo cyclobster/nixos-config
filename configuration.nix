ceca78dff760f18fac3afcf3d3182d51a71a07f4
Roger Finch <cyclobster@gmail.com>
2026-05-02 11:23:03 +0000

    fix font size

--- configuration.nix
+++ configuration.nix
@@ -71,6 +71,8 @@
 
   # programs.firefox.enable = true;
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
    environment.systemPackages = with pkgs; [
